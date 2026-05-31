51f263de6ac5f95ad5ed3d5093c1b67cd3655dcc
toni044 <tonyayitey17@gmail.com>
2026-05-31 21:16:46 +0000

    Final initials push

--- initials.py
+++ initials.py
@@ -2,6 +2,40 @@
 def main():
   pass #code goes here
 
+# File: initials.py
+# Description: Prints my initials in large stylized letters.
+# Assignment Number: 2
+#
+# Name: Anthony Kwao Ayitey
+# STUDENT ID: 2425404286
+# Email: tonyayitey17@gmail.com
+# Grader: Augustus Buckman
+
+
+
+def main():
+    # Prints my initials in large stylized letters
+
+    print()
+    print("...AAA")
+    print()
+
+    print("...AAAAAAAAAA****YYYYYYYYYY****AAAAAAAAAA")
+    print("...AAAA..AAAA****YY....YY****AAAA..AAAA")
+    print("...AAAA..AAAA****YY....YY****AAAA..AAAA")
+    print("...AAAA..AAAA****..YYYYYY****AAAA..AAAA")
+    print("...AAAAAAAAAA****....YY..****AAAAAAAAAA")
+    print("...AAAA..AAAA****....YY..****AAAA..AAAA")
+    print("...AAAA..AAAA****....YY..****AAAA..AAAA")
+    print("...AAAA..AAAA****....YY..****AAAA..AAAA")
+    print("...AAAA..AAAA****....YY..****AAAA..AAAA")
+    print("...AAAA..AAAA****....YY..****AAAA..AAAA")
+
+    print()
 
 
 main()
+
+main()
+
+
